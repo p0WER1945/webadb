f8db56cd84e5e5b1391d2168e5c4ab062ca53485
Block <cfn45@qq.com>
2025-12-08 23:43:26 +0800

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold underline">
+          Vite + React + Tailwindcss
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
